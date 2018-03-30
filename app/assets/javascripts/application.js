ebef85dec6837676df38dbf2b6b1caeb22804428
Brittany Morris <brittmmendez@gmail.com>
2018-03-30 13:25:38 +0000

    automatically backed up by learn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require wrong_place_wrong_time
+//= require application
+//= require hello_world
+//= require learn
+//= require main
+
